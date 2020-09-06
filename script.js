37d33b2ed6ec73449cdce8784d2157d2f3c9f23c
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-09-06 11:30:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ window.onload = () =>{
 
 
     const header = document.querySelector(".header");
+
+    counterContainer = document.getElementById("counter-container");
     
     
     document.body.onscroll = () => {  
@@ -24,8 +26,59 @@ window.onload = () =>{
             header.style.backgroundColor = "transparent";
         } 
 
+        if (document.documentElement.scrollTop > 128) {       
+            
+                counterContainer.style.opacity = "1";
+            
+            
+            counter();       
+        }
+
+
+
+    
+
+
         console.log(document.documentElement.scrollTop);
+
+
+
     }
+
+
+
+
+    var counter = function() {
+    $('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
+      
+        {
+      
+          duration: 8000,
+          easing:'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+      
+        });  
+        
+        
+      
+      });
+    }
+
+    
+    
+    
 
 
 
