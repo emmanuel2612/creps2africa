8c5fa848ec6933a6954117481c7c0cc499e42b58
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-09-18 11:18:56 +0100

    Add files via upload

--- header1.js
+++ header1.js
@@ -79,9 +79,24 @@
       header.style.backgroundColor = "transparent";
     }
 
-    if (document.documentElement.scrollTop > 128) {
+    if (document.documentElement.scrollTop > 300) {
 
       counterContainer.style.opacity = "1";
+
+      $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    
+    
+    
 
 
     }
@@ -126,20 +141,7 @@
 
 
 
-  $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
-
-
-
+ 
 
 
 }
